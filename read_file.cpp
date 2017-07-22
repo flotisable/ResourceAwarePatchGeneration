90cb8e831daf7771f4629930cd8a8603dcfd40a0
evan09900966 <evan09900966@gmail.com>
2017-07-23 02:00:37 +0800

    Create read_file.cpp
    創建 read_file.cpp

---
+++ read_file.cpp
@@ -0,0 +1,34 @@
+#include "ResourceAwarePatchGeneration.h"
+
+using namespace std;
+
+void ResourceAwarePatchGenerator::read_file()
+{   
+    cout<<"read_file ..."<<endl;
+
+    char Command[1000];
+    
+    /////////////////////////////////////////////////////////////////
+    //                           read F file                       //
+    /////////////////////////////////////////////////////////////////
+    sprintf( Command, "read %s", in_F_file.c_str() );
+    if ( Cmd_CommandExecute( pAbc, Command ) )
+    {
+        fprintf( stdout, "Cannot execute command \"%s\".\n", Command );
+    }
+    
+    initial_F = Abc_FrameReadNtk(pAbc);
+    cout <<"    F Po num:"<<Abc_NtkPoNum(initial_F) << endl;
+    
+    /////////////////////////////////////////////////////////////////
+    //                           read G file                       //
+    /////////////////////////////////////////////////////////////////
+    
+    sprintf( Command, "read %s", in_G_file.c_str() );
+    if ( Cmd_CommandExecute((Abc_Frame_t*) pAbc, Command ) )
+    {
+        fprintf( stdout, "Cannot execute command \"%s\".\n", Command );
+    }
+    initial_G = Abc_FrameReadNtk((Abc_Frame_t*)pAbc);
+    
+}
