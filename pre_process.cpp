95dd1dfa8a7f7bf1a0ba296d8f1766faf0f60076
evan09900966 <evan09900966@gmail.com>
2017-07-23 02:02:09 +0800

    Update pre_process.cpp
    add newline after cout

--- pre_process.cpp
+++ pre_process.cpp
@@ -3,19 +3,13 @@
 
 using namespace std;
 
-extern "C"
-{
-int    Cmd_CommandExecute( void * pAbc, char * sCommand );
-}
-
-
 void ResourceAwarePatchGenerator:: pre_process()
 {
-	Abc_Obj_t* iter_obj;
-	int i;
-	Abc_NtkForEachObj(initial_G, iter_obj,i)	
-	{
-		cout << Abc_ObjName(iter_obj);
-	}
+    Abc_Obj_t* iter_obj;
+    int i;
+    Abc_NtkForEachObj(initial_G, iter_obj,i)    
+    {
+        cout << Abc_ObjName(iter_obj)<<endl;
+    }
 
 }
