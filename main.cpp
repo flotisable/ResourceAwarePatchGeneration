cea8e7b7beb59fa66edcdd551f50830c4ed36bd1
evan09900966 <evan09900966@gmail.com>
2017-07-23 01:37:25 +0800

    Update main.cpp
    執行read_file與delete_unused_PO

--- main.cpp
+++ main.cpp
@@ -15,7 +15,10 @@ int main( int argc, char *argv[] )
 
   ResourceAwarePatchGenerator solver( argv[1], argv[2], argv[3], argv[4], argv[5] );
   solver.pre_process();
-
+  
+  solver.read_file();
+  solver.delete_unused_PO();
+  
   solver.construct_DLN();
   solver.functional_dependency();
   solver.write_patch();
