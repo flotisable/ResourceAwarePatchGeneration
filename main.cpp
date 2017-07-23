9af35d69c317792147a0bbecdb2365ddbcb35fc5
Evan <evan09900966@gmail.com>
2017-07-23 23:03:53 +0800

    swap function running order

--- main.cpp
+++ main.cpp
@@ -14,7 +14,12 @@ int main( int argc, char *argv[] )
   }
 
   ResourceAwarePatchGenerator solver( argv[1], argv[2], argv[3], argv[4], argv[5] );
-
+  
+  solver.read_file();
+  solver.delete_unused_PO();
+  solver.read_weight();
+  solver.pre_process();
+  
   solver.construct_DLN();
   solver.functional_dependency();
   solver.write_patch();
