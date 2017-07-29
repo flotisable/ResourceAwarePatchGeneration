7261178a35e05a6aa0f2517790248d40031509e1
flotisable <s09930698@gmail.com>
2017-07-29 17:24:32 +0800

    finish TestInterpolationEngine read function

--- Test/InterpolationEngine/TestInterpolationEngine.cpp
+++ Test/InterpolationEngine/TestInterpolationEngine.cpp
@@ -1,8 +1,22 @@
 #include "TestInterpolationEngine.h"
 
+#include <string>
+
 extern "C"
 {
 #include "base/main/main.h"
+#include "base/io/ioAbc.h"
+}
+
+Abc_Obj_t* findPo( Abc_Ntk_t *circuit, const std::string &name )
+{
+  Abc_Obj_t *po;
+  int       i;
+
+  Abc_NtkForEachPo( circuit, po, i )
+    if( std::string( Abc_ObjName( po ) ) == name ) return po;
+
+  return NULL;
 }
 
 TestInterpolationEngine::TestInterpolationEngine()
@@ -17,4 +31,21 @@ TestInterpolationEngine::~TestInterpolationEngine()
 
 void TestInterpolationEngine::test()
 {
+  using std::string;
+
+  const string inFile   = "test.v";
+  const string outFile  = "testOut.v";
+
+  read( inFile );
+}
+
+void TestInterpolationEngine::read( const std::string &file )
+{
+  dln = Io_ReadVerilog( const_cast<char*>( file.c_str() ), 0 );
+  dln = Abc_NtkStrash( dln, 1, 1, 0 );
+
+  targetFunction = findPo( dln, "out" );
+
+  baseFunctions.push_back( findPo( dln, "g1" ) );
+  baseFunctions.push_back( findPo( dln, "g2" ) );
 }
--- Test/InterpolationEngine/TestInterpolationEngine.h
+++ Test/InterpolationEngine/TestInterpolationEngine.h
@@ -1,7 +1,15 @@
 #ifndef TEST_INTERPOLATION_ENGINE_H
 #define TEST_INTERPOLATION_ENGINE_H
 
+#include <string>
+#include <vector>
+
 #include "SatAndInterpolation/InterpolationEngine.h"
+
+extern "C"
+{
+#include "base/abc/abc.h"
+}
 
 class TestInterpolationEngine
 {
@@ -14,7 +22,18 @@ class TestInterpolationEngine
 
   private:
 
+    void read ( const std::string &file );
+    void write( const std::string &file );
+
     InterpolationEngine engine;
+
+    Abc_Ntk_t               *dln;
+    Abc_Obj_t               *targetFunction;
+    std::vector<Abc_Obj_t*> baseFunctions;
+
+    Abc_Ntk_t *interpolant;
 };
+
+Abc_Obj_t* findPo( Abc_Ntk_t *circuit, const std::string &name );
 
 #endif
