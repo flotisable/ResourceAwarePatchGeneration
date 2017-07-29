b991b74879d0761076a2fc30807f02b28ce318b6
flotisable <s09930698@gmail.com>
2017-07-29 14:35:45 +0800

    modify InterpolationEngine to use NtkToCnfConverter

--- Makefile
+++ Makefile
@@ -19,7 +19,7 @@ export
 
 all: $(PROG)
 
-$(PROG): pre_process.o read_file.o delete_unused_PO.o trav_Po_add_to_set.o read_weight.o main.o ResourceAwarePatchGeneration.o write_patch.o $(satAndInterDir)/interpolation.o $(satAndInterDir)/NtkToCnfConverter.o $(abcLibDir)/$(abcLib) 
+$(PROG): pre_process.o read_file.o delete_unused_PO.o trav_Po_add_to_set.o read_weight.o main.o ResourceAwarePatchGeneration.o write_patch.o $(satAndInterDir)/interpolation.o $(satAndInterDir)/NtkToCnfConverter.o $(satAndInterDir)/InterpolationEngine.o $(abcLibDir)/$(abcLib) 
 	$(LD) $(LDFLAGS) -o $@ $^
 
 main.o: main.cpp
@@ -46,7 +46,7 @@ read_weight.o: read_weight.cpp ResourceAwarePatchGeneration.h
 write_patch.o: write_patch.cpp
 	$(CXX) $(CXXFLAGS) -c -o $@ $< 
 
-$(satAndInterDir)/interpolation.o $(satAndInterDir)/NtkToCnfConverter.o:
+$(satAndInterDir)/interpolation.o $(satAndInterDir)/NtkToCnfConverter.o $(satAndInterDir)/InterpolationEngine.o:
 	$(MAKE) -e -C $(satAndInterDir)
 
 debug: CXXFLAGS += -g -O
--- SatAndInterpolation/InterpolationEngine.cpp
+++ SatAndInterpolation/InterpolationEngine.cpp
@@ -13,81 +13,23 @@ InterpolationEngine::InterpolationEngine()
   dln           = NULL;
   mInterpolant  = NULL;
 
-  ntkA = ntkB = NULL;
-  cnfA = cnfB = NULL;
-
-  commonVariables = NULL;
-  sat_solver      = NULL;
-}
-
-void InterpolationEngine::splitInterpolationAB()
-{
-  if( !dln || !targetFunction ) return; // precondition
-
-  ntkA = dln;
-  ntkB = Abc_NtkDup( ntkA );
-
-  Abc_NodeComplement( Abc_ObjCopy( targetFunction ) );
-
-  // save the copy of base function in networks B
-  for( int i = 0 ; i < baseFunctions.size() ; ++i )
-  {
-     baseCopy.push_back( Abc_ObjCopy( baseFunctions[i] ) );
-  }
-  // end save the copy of base function in networks B
+  satSolver = NULL;
 }
 
 void InterpolationEngine::circuitToCnf()
 {
-  if( !ntkA || !ntkB ) return; // precondition
+  if( !dln || !targetFunction || baseFunctions.empty() ) return; // precondition
 
-  Aig_Obj_t *node;
-  int       i;
+  converter.setCircuit       ( dln             );
+  converter.setTargetFunction( targetFunction  );
+  converter.setBaseFunctions ( baseFunctions   );
 
-  Aig_Man_t *aigA = Abc_NtkToDar( ntkA, 0, 0 );
-  Aig_Man_t *aigB = Abc_NtkToDar( ntkB, 0, 0 );
-
-  cnfA = Cnf_DeriveSimple( Aig_ManCoNum( aigA ) );
-  cnfB = Cnf_DeriveSimple( Aig_ManCoNum( aigB ) );
-
-  Cnf_DataLift( cnfB, cnfA->nVars );
-
-  // save common variables
-  Aig_ManForEachObj( aigA, node, i )
-  {
-    for( int j = 0 ; j < baseFunctions.size() ; ++j )
-    {
-       if( node == baseFunctions[j] )
-         goto matched;
-    }
-    continue;
-
-matched:
-
-    Vec_Int_Push( commonVariables, cnfA->pVarNums[node->Id] );
-  }
-  // end save common variables
-
-  // save common variables copy
-  Aig_ManForEachObj( aigB, node, i )
-  {
-    for( int j = 0 ; j < baseCopy.size() ; ++j )
-    {
-       if( node == baseCopy[j] )
-         goto matchedCopy;
-    }
-    continue;
-
-matchedCopy:
-
-    Vec_Int_Push( commonCopy, cnfB->pVarNums[node->Id] );
-  }
-  // end save common variables
+  converter.convert();
 }
 
 void InterpolationEngine::addClauseA()
 {
-  if( !cnfA ) return; // precondition
+  if( !converter.cnfOn() ) return; // precondition
 
   int *begin;
   int *end;
@@ -97,11 +39,11 @@ void InterpolationEngine::addClauseA()
   satSolver = sat_solver_new();
 
   sat_solver_store_alloc( satSolver );
-  sat_solver_setnvars   ( satSolver, cnfA->nVars + cnfB->nVars );
+  sat_solver_setnvars   ( satSolver, converter.cnfOn()->nVars + converter.cnfOff()->nVars );
   // end setup sat solver
 
   // add clause A
-  Cnf_CnfForClause( cnfA, begin, end, i )
+  Cnf_CnfForClause( converter.cnfOn(), begin, end, i )
   {
     sat_solver_addclause( satSolver, begin, end );
   }
@@ -111,7 +53,7 @@ void InterpolationEngine::addClauseA()
 
 void InterpolationEngine::addClauseB()
 {
-  if( !cnfB || !satSolver ) return; // precondition
+  if( !converter.cnfOff() || !satSolver ) return; // precondition
 
   int *begin;
   int *end;
@@ -119,7 +61,7 @@ void InterpolationEngine::addClauseB()
   int lits[3];
 
   // add clause B
-  Cnf_CnfForClause( cnfB, begin, end, i )
+  Cnf_CnfForClause( converter.cnfOff(), begin, end, i )
   {
     sat_solver_addclause( satSolver, begin, end );
   }
@@ -128,26 +70,19 @@ void InterpolationEngine::addClauseB()
   // add common variables clause
   for( int i = 0 ; i < baseFunctions.size() ; ++i )
   {
-     lits[0] = toLitCond( Vec_IntEntry( commonVariables, i ), 0 );
-     lits[1] = toLitCond( Vec_IntEntry( commonCopy,      i ), 1 );
+     lits[0] = toLitCond( converter.literalsOn ()[i+1], 0 );
+     lits[1] = toLitCond( converter.literalsOff()[i+1], 1 );
 
-     sat_solver_addclause( satSolver, lit, lit + 2 );
+     sat_solver_addclause( satSolver, lits, lits + 2 );
 
-     lits[0] = toLitCond( Vec_IntEntry( commonVariables, i ), 1 );
-     lits[1] = toLitCond( Vec_IntEntry( commonCopy,      i ), 0 );
+     lits[0] = toLitCond( converter.literalsOn ()[i+1], 1 );
+     lits[1] = toLitCond( converter.literalsOff()[i+1], 0 );
 
-     sat_solver_addclause( satSolver, lit, lit + 2 );
+     sat_solver_addclause( satSolver, lits, lits + 2 );
   }
   // end add common variables clause
 
   sat_solver_store_mark_roots( satSolver );
-
-  // free the memory
-  Aig_ManStop ( aigA );
-  Aig_ManStop ( aigB );
-  Cnf_DataFree( cnfA );
-  Cnf_DataFree( cnfB );
-  // end free the memory
 }
 
 void InterpolationEngine::interpolation()
