23f60c093b32ab57a500d2d845d43e9e2a789d5e
JC22 <b02901136@ntu.edu.tw>
2017-08-01 20:42:22 +0800

    sa part start

--- sat_solving.cpp
+++ sat_solving.cpp
@@ -1,12 +1,17 @@
 #include <ResourceAwarePatchGeneration.h>
 #include <iostream>
 #include "SatAndInterpolation/InterpolationEngine.h"
+#include <time.h>
 
 using namespace std;
 
 
-void ResourceAwarePatchGenerator::sat_solve()
+
+void ResourceAwarePatchGenerator::simulated_annealing ()
 {
+	cout << "[INFO] CNF construct started " << endl;
+	cout << clock() << endl;
+	
 	NtkToCnfConverter* cnf_converter=new NtkToCnfConverter();	
 	vector<Abc_Obj_t*> base;
 	for (int i=0;i<gate_list.size();i++)
@@ -39,29 +44,54 @@ void ResourceAwarePatchGenerator::sat_solve()
      		lits[1] = toLitCond( cnf_converter->literalsOff()[i+1], 1 );
 		lits[2]=  toLitCond( on_off_total_var_num+i,1);
 
-     		if( !sat_solver_addclause( solver, lits, lits + 2 ) )
+     		if( !sat_solver_addclause( solver, lits, lits + 3 ) )
        			std::cout << "clause add error!\n";
 
      		lits[0] = toLitCond( cnf_converter->literalsOn ()[i+1], 1 );
      		lits[1] = toLitCond( cnf_converter->literalsOff()[i+1], 0 );
 		lits[2]=  toLitCond( on_off_total_var_num+i,1);
 
-     		if( !sat_solver_addclause( solver, lits, lits + 2) )
+     		if( !sat_solver_addclause( solver, lits, lits + 3) )
        			std::cout << "clause add error!\n";
 	}
+	cout << "[INFO] CNF construct finished, SA started" << endl;
 
-
-
-
-
-	int base_alpha[gate_list.size()];
-	for (int i=0;i<gate_list.size();i++)
+	vector<bool> current_solution(gate_list.size());
+	for (int i=0;i<current_solution.size();++i)
 	{
-		base_alpha[i]=toLitCond(on_off_total_var_num+i,0);
+		current_solution[i]=true;
 	}
-	int status=sat_solver_solve((sat_solver*) solver,base_alpha,base_alpha+gate_list.size(),10000,0,0,0);
-	cout << status << endl;
 	
+	best_used_base=current_solution;
+
+	
+	
+} 
+
+bool sat_solve(void* solver,vector<bool>& base_selection , vector<Weight_gate*>& gate_list,int on_off_total_var_num)
+{
+
+
+
+
+	int count=0;
+	for (int i=0;i<base_selection.size();++i)
+	{
+		if(base_selection[i]==true)
+			count++; 
+	}
+	int base_alpha[count];
+	count=0;
+	for (int i=0;i<gate_list.size();++i)
+	{
+		if(base_selection[i]==true)
+		{
+			base_alpha[count]=toLitCond(on_off_total_var_num+i,0);
+			count++;
+		}
+	}
+	int status=sat_solver_solve((sat_solver*) solver,base_alpha,base_alpha+count,10000,0,0,0);
+ 	return status;	
 	
 
 	
